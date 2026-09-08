9ab44d7ee3c4b61ccf77d59c099cad57e23ad5c2
Mayur Zope <zopemayur666@gmail.com>
2026-09-08 22:51:12 +0530

    Remove Practice50lpa folder

--- Practice50lpa/algorithm.h
+++
@@ -1,7 +0,0 @@
-// algorithm.h
-#ifndef ALGORITHM_H // guard
-#define ALGORITHM_H
-
-//function ////
-void sort(int arr[], int n); //declaration
-#endif // ALGORITHM_H
--- Practice50lpa/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "algorithm.h"
-using namespace std;
-int main()
-{
-    int arr[] = {1, 2, 5, 7, 6, 2};
-    sort(arr, 6);
-    return 0;
-}
